dcca673dcf72e819cb46ad096563b9e3fbc47314
evera8 <evera8@hotmail.com>
2023-05-15 18:03:28 -0300

    subo primera parte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ThankyouComponent } from './components/thankyou/thankyou.component';
+import { ProductComponent } from './components/product/product.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    ProductsComponent,
+    ThankyouComponent,
+    ProductComponent,
+    CheckoutComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
